8d675ce4c4144a6e8c9e4969a3a30e193e35e344
İdil Öncü <oncuidil@gmail.com>
2024-03-07 23:28:51 +0300

    mvvm yapisindaki databinding islemleri yapildi.

--- app/src/main/java/com/example/mvvmkullanimi/MainActivity.kt
+++ app/src/main/java/com/example/mvvmkullanimi/MainActivity.kt
@@ -11,31 +11,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //binding= ActivityMainBinding.inflate(layoutInflater)
        binding=DataBindingUtil.setContentView(this,R.layout.activity_main   )
+        binding.mainActivityNesnesi=this
 
-        binding.textViewSonuc.text="0"
-        binding.buttonToplama.setOnClickListener{
-           val alinanSayi1= binding.editTextTextSayi1.text.toString()
-            val alinanSayi2=binding.editTextTextSayi2.text.toString()
+        binding.hesaplamaSonucu="0"
 
-            val sayi1 = alinanSayi1.toInt()
-            val sayi2 = alinanSayi2.toInt()
+    }
+    fun buttonToplamaTikla(alinanSayi1:String,alinanSayi2:String){
 
-            val toplam = sayi1+sayi2
-            binding.textViewSonuc.text=toplam.toString()
-        }
-        binding.buttonCarpma.setOnClickListener{
-            val alinanSayi1C= binding.editTextTextSayi1.text.toString()
-            val alinanSayi2C=binding.editTextTextSayi2.text.toString()
+        val sayi1 = alinanSayi1.toInt()
+        val sayi2 = alinanSayi2.toInt()
 
-            val sayi1C = alinanSayi1C.toInt()
-            val sayi2C = alinanSayi2C.toInt()
+        val toplam = sayi1+sayi2
+        binding.hesaplamaSonucu=toplam.toString()
 
-            val carpma = sayi1C*sayi2C
-            binding.textViewSonuc.text=carpma.toString()
+    }
+    fun buttonCarpmaTikla(alinanSayi1: String,alinanSayi2: String){
+        val sayi1C = alinanSayi1.toInt()
+        val sayi2C = alinanSayi2.toInt()
 
-        }
-
-
-
+        val carpma = sayi1C*sayi2C
+        binding.hesaplamaSonucu=carpma.toString()
     }
 }
